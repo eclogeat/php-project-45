19ff23332d8bd546d86f46e56420b4732ae27eca
eclogeat <eclogeat@gmail.com>
2024-08-20 17:55:02 +0700

    4 step is doone

--- src/Cli.php
+++ src/Cli.php
@@ -1,15 +1,30 @@
 <?php
 
+/**
+ * This file contains the Cli class and related functions for the Brain Game project.
+ *
+ * @category BrainGame
+ * @package  Hexlet\Code
+ * @author   eclogeat <eclogeat@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/eclogeat/php-project-45
+ */
+
 namespace Hexlet\Code;
+
 require_once __DIR__ . "/../vendor/autoload.php";
 
 use function cli\line;
 use function cli\prompt;
 
-function greetings() {
+/**
+ * Greets the user and asks for their name.
+ *
+ * @return void
+ */
+function greetings()
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-
 }
-
